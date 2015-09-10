180ddf1bd42447d8cbba963135ec2824f47b779c
Wiliam Souza <wiliamsouza83@gmail.com>
2015-09-09 21:01:51 -0300

    Add add form

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
 
-from forms import SearchForm
+from flask.ext.elasticsearch import FlaskElasticsearch
+
+from forms import SearchForm, AddForm
 
 
 app = Flask(__name__)
+es = FlaskElasticsearch(app)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super_secret_key'
 
@@ -20,9 +23,22 @@ def search():
     return render_template('search.html', form=form)
 
 
-@app.route("/add/")
+@app.route("/add/", methods=['GET', 'POST'])
 def add():
-    return "Add"
+    form = AddForm()
+    if form.validate_on_submit():
+        data = {
+            'name': form.name.data,
+            'key': form.key.data,
+            'value': form.value.data,
+        }
+        response = es.index(index='resistor', doc_type='component',
+                            body=data)
+        if not response['created']:
+            return 'Error'
+        else:
+            return 'Success'
+    return render_template('add.html', form=form)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -5,3 +5,10 @@ from wtforms import TextField, validators
 
 class SearchForm(Form):
     search = TextField('Search', [validators.InputRequired()])
+
+
+class AddForm(Form):
+    name = TextField('Name', [validators.InputRequired()])
+    key = TextField('Key', [validators.InputRequired()])
+    value = TextField('Value', [validators.InputRequired()])
+
